4fe09d1612f0e0e651149bd2eadbe37f0ee6d5ae
Gagan Chawara <gaganchawara12@gmail.com>
2023-09-16 22:32:15 +0530

    adds logger pkg

---
+++ pkg/logger/logger.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"context"
+	"github.com/gaganchawara/loans/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"os"
+	"time"
+)
+
+type contextKey int
+
+const (
+	CtxKey contextKey = iota
+)
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: time.RFC3339Nano,
+	})
+	logrus.SetOutput(os.Stdout)
+	logrus.AddHook(&StackTraceHook{})
+}
+
+func Get(ctx context.Context) *logrus.Entry {
+	if ctx == nil {
+		return newStdLogger()
+	}
+
+	k, ok := ctx.Value(CtxKey).(*logrus.Entry)
+	if ok {
+		return k
+	} else {
+		return newStdLogger().WithContext(ctx)
+	}
+}
+
+func newStdLogger() *logrus.Entry {
+	return logrus.NewEntry(logrus.StandardLogger())
+}
---
+++ pkg/logger/stacktrace.go
@@ -0,0 +1,18 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+	"runtime/debug"
+)
+
+type StackTraceHook struct{}
+
+func (hook *StackTraceHook) Levels() []logrus.Level {
+	return []logrus.Level{logrus.PanicLevel, logrus.FatalLevel, logrus.ErrorLevel}
+}
+
+func (hook *StackTraceHook) Fire(entry *logrus.Entry) error {
+	stack := debug.Stack()
+	entry.Data["stack_trace"] = string(stack)
+	return nil
+}
