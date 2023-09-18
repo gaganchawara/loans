cedb7ae1600c90bc793730015eb23845a044d5a9
Gagan Chawara <gaganchawara12@gmail.com>
2023-09-18 18:47:58 +0530

    adds repository

---
+++ internal/loans/interfaces/interfaces.go
@@ -0,0 +1,17 @@
+package interfaces
+
+import (
+	"context"
+	"github.com/gaganchawara/loans/internal/loans/entity"
+	"github.com/gaganchawara/loans/pkg/errors"
+)
+
+type Service interface {
+}
+
+type Repository interface {
+	CreateLoan(ctx context.Context, loan *entity.Loan) errors.Error
+	UpdateLoan(ctx context.Context, loan *entity.Loan) errors.Error
+	LoadLoan(ctx context.Context, loanId string) (*entity.Loan, errors.Error)
+	LoadRepayment(ctx context.Context, id string) (*entity.Repayment, errors.Error)
+}
---
+++ internal/loans/repository/repo.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"context"
+	"github.com/gaganchawara/loans/internal/errorcode"
+	"github.com/gaganchawara/loans/internal/loans/entity"
+	"github.com/gaganchawara/loans/pkg/errors"
+	"gorm.io/gorm"
+)
+
+type repo struct {
+	db *gorm.DB
+}
+
+type Repository interface{}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repo{
+		db: db,
+	}
+}
+
+func (r repo) CreateLoan(ctx context.Context, loan *entity.Loan) errors.Error {
+	q := r.db.Table(loan.TableName()).Create(loan)
+	if q.Error != nil {
+		return errors.New(ctx, errorcode.InternalServerError, q.Error).Report()
+	}
+
+	return nil
+}
+
+func (r repo) UpdateLoan(ctx context.Context, loan *entity.Loan) errors.Error {
+	q := r.db.Table(loan.TableName()).Updates(loan)
+	if q.Error != nil {
+		return errors.New(ctx, errorcode.InternalServerError, q.Error).Report()
+	}
+
+	return nil
+}
+
+func (r repo) LoadLoan(ctx context.Context, loanId string) (*entity.Loan, errors.Error) {
+	var loan entity.Loan
+	q := r.db.Table(loan.TableName()).Where("id = ?", loanId).Where("deleted_at IS NULL").First(&loan)
+	if q.Error != nil {
+		if q.Error == gorm.ErrRecordNotFound {
+			return nil, nil
+		} else {
+			return nil, errors.New(ctx, errorcode.InternalServerError, q.Error).Report()
+		}
+	}
+
+	return &loan, nil
+}
+
+func (r repo) LoadRepayment(ctx context.Context, id string) (*entity.Repayment, errors.Error) {
+	var repayment entity.Repayment
+	q := r.db.Table(repayment.TableName()).Where("id = ?", id).Where("deleted_at IS NULL").
+		First(&repayment)
+	if q.Error != nil {
+		if q.Error == gorm.ErrRecordNotFound {
+			return nil, nil
+		} else {
+			return nil, errors.New(ctx, errorcode.InternalServerError, q.Error).Report()
+		}
+	}
+
+	return &repayment, nil
+}
